27cacb4ee9c9e969280c29d64c1ed6f63b0e4f12
Fayaz-DS <userc5630@gmail.com>
2025-12-25 14:57:44 +0530

    FIx API

--- src/lib/sanity.js
+++ src/lib/sanity.js
@@ -1,7 +1,7 @@
 import { createClient } from "next-sanity";
 
 export const client = createClient({
-  projectId: "YOUR_PROJECT_ID",
+  projectId: "hr00zsfp",
   dataset: "production",
   apiVersion: "2024-01-01",
   useCdn: true
